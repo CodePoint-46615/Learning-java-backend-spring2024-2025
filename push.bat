48846b63c42080a745c0498e8a117d7371815ccd
SAIKOT KUNDU <codepoint46615@gmail.com>
2025-05-06 04:05:34 +0600

    Delete push.bat

--- push.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "D:\OneDrive - American International University-Bangladesh\0. 10th Semester\Advance Programming with Java\Learning-java-backend"
-git pull origin main --rebase 
-git add --all 
-git commit -m "Auto Commit - %date% %time%"
-git push origin main
